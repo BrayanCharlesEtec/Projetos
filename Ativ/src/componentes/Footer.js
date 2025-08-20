9bf757f55720d42bf0d7beea330f5e524cdd8c75
bryynnn <161508666+BrayanCharlesEtec@users.noreply.github.com>
2025-08-20 14:39:40 -0300

    Update Footer.js

--- Ativ/src/componentes/Footer.js
+++ Ativ/src/componentes/Footer.js
@@ -4,9 +4,10 @@ function Footer() {
   const ano = new Date().getFullYear();
   return (
     <footer style={{ background: "#333", padding: "10px", color: "white", textAlign: "center" }}>
-      <p>ETEC MCM</p>
+      <p>ETEC MCM - {ano}</p>
     </footer>
   );
 }
 
 export default Footer;
+
